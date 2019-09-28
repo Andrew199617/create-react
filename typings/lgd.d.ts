7e26d0d226cfe2720f00b524770a711d52b3152e
Andrew Velez Alvarez (Insight Global Inc) <v-mavele@microsoft.com>
2019-09-27 18:50:30 -0700

    fixed docs.

--- typings/lgd.d.ts
+++ typings/lgd.d.ts
@@ -1,19 +1,23 @@
 
-/**
-* @description Public facing class.
-* @type {LGDType}
-*/
-declare interface LGDType {
-	/**
-  * @description Polyfill Object with super to be able to use super when using OLOO.
+declare module '@learngamedevelopment/oloo' {
+  /**
+  * @description Public facing class.
+  * @type {LGDType}
   */
-	polyfill(): any;
+  const LGDType: {
+    /**
+    * @description Polyfill Object with super to be able to use super when using OLOO.
+    */
+    polyfill(): void;
 
-	/**
-   * @description Assign and Object the same as Object.Assign but works with getters, setters and inheritance.
-   * @param {Object} target The target object we are inheriting from gets modified in place.
-   * @param {Object[]} sources The objects to to apply
-   * @returns {Object} The new object.
-   */
-	ObjectAssign(target: Object, ...sources: any): Object;
+    /**
+     * @description Assign and Object the same as Object.Assign but works with getters, setters and inheritance.
+     * @param {Object} target The target object we are inheriting from gets modified in place.
+     * @param {Object[]} sources The objects to to apply
+     * @returns {Object} The new object.
+     */
+    ObjectAssign(target: Object, ...sources: any): Object;
+  }
+
+  export = LGDType;
 }
