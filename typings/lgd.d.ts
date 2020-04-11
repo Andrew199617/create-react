54eef4af5b28f6fffdb9bac7c9b636025384a9c3
Andrew Velez <andrewmauro199617@yahoo.com>
2020-04-11 11:05:04 -0700

    Improved typings file.

--- typings/lgd.d.ts
+++ typings/lgd.d.ts
@@ -1,6 +1,8 @@
 
 declare module '@mavega/react' {
 
+  import { ClassicComponentClass } from "react";
+
   /**
   * @description Create-React-Classes
   */
@@ -8,7 +10,7 @@ declare module '@mavega/react' {
 
     /**
      * @description Create a React Component using an Object.
-     * 
+     *
      * Usage:
      * ``` js
      * const obj = Object.create({});
@@ -16,23 +18,23 @@ declare module '@mavega/react' {
      * createReactComponent(obj);
      * ```
      * For More detailed example see ReadMe.
-     * @param {obj} objectToWrap 
+     * @param {React.Component<P, S>} objectToWrap The object to create a React.Component from.
      */
-    createReactComponent<obj>(objectToWrap: obj) : obj;
+    createReactComponent<P, S = {}>(objectToWrap: React.Component<P, S>) : ClassicComponentClass<P>;
 
     /**
      * @description Create a Pure React Component using an Object.
      * @see https://reactjs.org/docs/react-api.html#reactpurecomponent
-     * 
+     *
      * Usage:
      * ``` js
      * const obj = Object.create({});
      * createReactComponent(obj);
      * ```
      * For More detailed example see ReadMe.
-     * @param {obj} objectToWrap 
+     * @param {React.PureComponent<P, S>} objectToWrap The object to create a React.PureComponent from.
      */
-    createReactPure<obj>(objectToWrap: obj) : obj;
+    createReactPure<P, S = {}>(objectToWrap: React.PureComponent<P, S>): ClassicComponentClass<P>;
   }
 
   export = CreateReact;
